7685d09a0dce7e8b28d01d56ee5ed742c2230235
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2024-01-25 15:56:54 +0530

    Update index.js

--- src/EmployeesList/index.js
+++ src/EmployeesList/index.js
@@ -65,7 +65,7 @@ const handleDelete = async (employeeID) => {
         </tr>
     </thead>
     <tbody>
-    {employees.length ?? employees.map((employee, index) => (
+    {employees.length ? employees.map((employee, index) => (
                         <tr key={index}>
                             <td>{employee.name}</td>
                             <td>{employee.email}</td>
@@ -78,7 +78,7 @@ const handleDelete = async (employeeID) => {
                                 <button className="btn btn-link" onClick={() => handleDelete(employee._id)}>DELETE</button>
                             </td>
                         </tr>
-                    ))}
+                    )):<></>}
     </tbody>
 </table>
 
